8d379c00700d8fb72dd7abc79a4e20586009e689
lihaidehaoren <529509927@qq.com>
2022-12-25 10:21:39 +0800

    Update Membertest.java

--- Membertest.java
+++ Membertest.java
@@ -1,33 +1,25 @@
 public class Membertest {
     public static void main(String[] args){
         // Create a SilverCardMember object
-        SilverCardMember member = new SilverCardMember("H01 F 35");
-        GoldCardMember member1 =new GoldCardMember("H01 F 35");
+        GoldCardMember member1 = new GoldCardMember("M01 M 28");
+        System.out.println(member1.consume(1500)); // 1500
+        System.out.println(member1.consume(3000)); // 2950
+        System.out.println(member1.consume(8000)); // 7550
+        System.out.println(member1.consume(12000)); // 11050
+        System.out.println(member1.consume(40000)); // 33850
+        System.out.println(member1.getTotalCost()); // 33850
+        System.out.println(member1.getGenderAgeCost()); // M01 M 28 33850
+        System.out.println(member1.toString()); // GoldCardMember: M01 M 28
 
-// Calculate the actual price after applying the discount for an item with an original price of 2000
-        double actualPrice = member.consume(2000); // The actual price should be 2000
-
-// Calculate the actual price after applying the discount for an item with an original price of 3000
-        actualPrice = member.consume(3000); // The actual price should be 2934
-
-// Calculate the actual price after applying the discount for an item with an original price of 6000
-        actualPrice = member.consume(6000); // The actual price should be 5900
-
-// Calculate the actual price after applying the discount for an item with an original price of 3000
-        actualPrice = member.consume(3000); // The actual price should be 2800
-
-// Calculate the actual price after applying the discount for an item with an original price of 4000
-        actualPrice = member.consume(4000); // The actual price should be 3850
-
-// Calculate the actual price after applying the discount for an item with an original price of 50
-        actualPrice = member.consume(50); // The actual price should be 0
-
-// Get the member's information and the points owned by the member
-        String info = member.getGenderAgeCost(); // The string should be "H01 F 35 151.0"
-        System.out.println(info);
-
-// Get the string representation of the member
-        String str = member.toString(); // The string should be "SilverCardMember: H01 F 35 points=151.0"
-        System.out.println(str);
-    }
+        SilverCardMember member2 = new SilverCardMember("H01 F 35");
+        System.out.println(member2.consume(2000)); // 2000
+        System.out.println(member2.consume(3000)); // 2934
+        System.out.println(member2.consume(6000)); // 5900
+        System.out.println(member2.consume(3000)); // 2800
+        System.out.println(member2.consume(4000)); // 3850
+        System.out.println(member2.consume(50)); // 0
+        System.out.println(member2.getTotalCost()); // 19950
+        System.out.println(member2.getGenderAgeCost()); // H01 F 35 19950
+        System.out.println(member2.toString()); // SilverCardMember: H01 F 35 points=151
+        }
 }
